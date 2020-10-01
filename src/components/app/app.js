5418eea90187609c3d0a0cc81f8eab250fd5b679
Yaroslav <yaroslav.zhuvanov.ddi@gmail.com>
2020-10-01 10:49:11 +0300

    optional params

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -48,7 +48,8 @@ export default class App extends Component {
               <Route path="/people"
                      exact
                      render={() => <h2>People</h2>}/>
-              <Route path="/people" component={PeoplePage}/>
+              <Route path="/people/:id?"
+                     component={PeoplePage}/>
               <Route path="/planets" component={PlanetsPage}/>
               <Route path="/starships" exact component={StarshipsPage}/>
               <Route path="/starships/:id"
